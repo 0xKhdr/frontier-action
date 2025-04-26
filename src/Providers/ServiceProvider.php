cd5556980c1ee10ac11a04b8141ea965ba3cb99d
0xKhdr <0xkhdr@gmail.com>
2025-04-26 16:07:01 +0300

    Register make action command

--- src/Providers/ServiceProvider.php
+++ src/Providers/ServiceProvider.php
@@ -2,11 +2,19 @@
 
 namespace Frontier\Actions\Providers;
 
+use Frontier\Actions\Console\Commands\MakeAction;
 use Illuminate\Support\ServiceProvider as IlluminateServiceProvider;
 
 class ServiceProvider extends IlluminateServiceProvider
 {
+    protected array $commands = [
+        MakeAction::class,
+    ];
+
     public function register(): void {}
 
-    public function boot(): void {}
+    public function boot(): void
+    {
+        $this->commands($this->commands);
+    }
 }
